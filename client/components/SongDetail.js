9068f4742d72d4891134b4ecad0cf373b63c2d53
Pierre Cruz <contact@pierrecruz.com>
2023-02-26 10:30:57 +0100

    #68

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { graphql } from "react-apollo";
 import { Link } from "react-router";
 
+import LyricCreate from "./LyricCreate";
+import LyricList from "./LyricList";
 import fetchSong from "../queries/fetchSong";
 
 class SongDetail extends Component {
@@ -16,6 +18,8 @@ class SongDetail extends Component {
       <div>
         <Link to="/">Back</Link>
         <h3>{song.title}</h3>
+        <LyricList lyrics={song.lyrics} />
+        <LyricCreate songId={this.props.params.id} />
       </div>
     );
   }
